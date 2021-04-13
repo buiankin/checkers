87ba517484e16fd86bf58251f609d970963077ab
Denis Buiankin <buiankin@yandex.ru>
2021-04-13 17:46:13 +0700

    test

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,6 @@
+import Game from './ai/game';
+import Computer from './ai/computer';
+import Move from './ai/move';
 
 
 export const initialState = { 
@@ -11,15 +14,16 @@ export const initialState = {
   selectedItemRow: 0, selectedItemColumn: 0,
 
   playerTurn: 1,
+  // 1 -> player 1 normal pieces, 2 -> player 2 normal pieces, 3 -> player 1 kings, 4 -> player 2 kings
   gameBoard : [
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 11, 0, 21, 0, 31, 0, 41],
+    [51, 0, 61, 0, 71, 0, 81, 0],
+    [0, 91, 0, 103, 0, 111, 0, 121],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 2, 0, 2, 0, 2, 0],
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0]
+    [134, 0, 142, 0, 152, 0, 162, 0],
+    [0, 172, 0, 182, 0, 192, 0, 202],
+    [212, 0, 222, 0, 232, 0, 242, 0]
   ],
   //arrays to store the instances
   pieces : [],
@@ -194,10 +198,72 @@ export const reducer = (state: State, action: Action) => {
 
 
     case "test":
+    {
+      let checkers = new Game();
+      let computerPlayer = new Computer(state.playerTurn, 1);
+      // Важно, чтобы -1 заполнились там, где нельзя ничего ставить
+      checkers.newGame();
+      // Текущее состояние
+      checkers.setCurrentState(state.gameBoard, state.playerTurn, 1);
+
+      let legalMoves = checkers.getLegalMoves(checkers.board);
+      if (legalMoves.length===0) {
+        console.log("I am lost!");
+          //gameOver = true;
+          //console.log("Player 2 Wins!");
+          //continue;
+      } else {
+      //console.log("Player 1's Turn: ");
+      //checkers.printListMoves(legalMoves);
+      //if (player1IsComputer){
+          let move = computerPlayer.alphaBetaSearch(checkers);
+          if (move)
+          {
+            checkers.applyMove(move, checkers.board);
+            console.log("Player Chose: ");
+            checkers.printMove(move);
+
+            let newBoard: number[][]=[];
+            state.gameBoard.forEach(line => {
+             newBoard.push(line.slice());
+            });
+
+            if (move.listCaptureCol.length>0)
+            {
+              for (let i=0; i<move.listCaptureCol.length; i++)
+              {
+                let row=move.listCaptureRow[i];
+                let col=move.listCaptureCol[i];
+                newBoard[row][col] = 0;
+              }
+
+            }
+       
+            // это для теста, чтобы можно было двигать любые фигуры
+            let color=newBoard[move.startRow][move.startCol];
+            newBoard[move.startRow][move.startCol] = 0;
+            newBoard[move.endRow][move.endCol] = color;// state.playerTurn;
+       
+             return {
+               ...state,
+               gameBoard: newBoard,
+               playerTurn: state.playerTurn===1?2:1
+             };
+       
+
+          }
+      }
+      //}
+      //checkers.printNote();
+      //checkers.printBoard();
+
+    
+
       return {
         ...state,
         playerTurn: state.playerTurn===1?2:1
       };
+    }
 
     default:
       throw new Error();
